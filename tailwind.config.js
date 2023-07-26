a438b5f98c5f7efe1fcbacec4ab2ba73b31680d1
Christian <duoduchristian36@gmail.com>
2023-07-26 08:56:56 +0000

    Interactivity with js
    added smtp to able to submit form, added a stucky navbar and animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,7 @@ module.exports = {
         "color-primary" : "#01051e",
         "color-primary-light" : "#020726",
         "color-primary-dark" : "#010417",
-        "color-secondary" : "#ff7d3b",
+        "color-secondary" : "#38bdf8",
         "color-gray" : "#333",
         "color-white" : "#fff",
         "color-blob" : "#A427DF",
